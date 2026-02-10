ec3ac9b21f6026862e2ede19281747fa288c5fd4
Karan Bisht <karanbisht4041@gmail.com>
2026-02-10 09:49:37 +0530

    dsa

--- Patterns/Butterfly.java
+++ Patterns/Butterfly.java
@@ -34,10 +34,12 @@ public class Butterfly {
             for(int j=1;j<=i ; j++){
             System.out.print("*");
         }
+
         //space
         for(int j =1; j<=2*(n-i);j++){
             System.out.print(" ");
         }
+        
         //stars 
         for(int j =1; j<=i;j++){
         System.out.print("*");
