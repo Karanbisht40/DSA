d0f7529fce72d62945764218dd7cd06736f17a81
Karan Bisht <karanbisht4041@gmail.com>
2026-02-14 21:00:00 +0530

    new

--- recursion/PrintNumberUsingrecursion.java
+++ recursion/PrintNumberUsingrecursion.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 public class PrintNumberUsingrecursion {
-
     public static void printNumber(int n) {
         if (n <= 0) {
             return;
@@ -9,7 +8,6 @@ public class PrintNumberUsingrecursion {
         System.out.println(n);
         printNumber(n - 1);
     }
-
     public static void main(String[] args) {
         printNumber(5);
 
