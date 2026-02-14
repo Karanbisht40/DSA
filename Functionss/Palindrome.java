1e6981a2e465ed6ddfc731ad3b89e50490887139
Karan Bisht <karanbisht4041@gmail.com>
2026-02-14 21:04:43 +0530

    new

--- Functionss/Palindrome.java
+++ Functionss/Palindrome.java
@@ -1,3 +1,17 @@
 public class Palindrome {
-    
+    public static boolean isPalindrome(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        sb.reverse();
+        String reversedStr = sb.toString();
+        return str.equals(reversedStr);
+    }
+
+    public static void main(String[] args) {
+        String str = "madam";
+        if (isPalindrome(str)) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+    }   
 }
