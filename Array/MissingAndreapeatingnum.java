ebd44b2170fb261b4715cdffa4ee3becacc41223
Karan Bisht <karanbisht4041@gmail.com>
2026-02-27 10:09:29 +0530

    chn

---
+++ Array/MissingAndreapeatingnum.java
@@ -0,0 +1,31 @@
+
+//bettrer sol--
+// public class MissingAndreapeatingnum {
+  
+//     public int[] findMissingRepeatingNumbers(int[] nums) {
+//           int n = nums.length;
+//     int[] hash = new int[n+1];
+//     for(int i =0; i<nums.length; i++){
+//         hash[nums[i]]++;
+//     }
+//     int repeating =-1; int missing= -1;
+//     for(int i =0; i<=n; i++){
+//         if(hash[i]==2){
+//             repeating =i;
+//         }else if(hash[i]==0){
+//             missing =i;
+//         }
+//         if(missing != -1 && repeating !=-1){
+//             break;
+//         }
+//     }
+//     return new int[]{repeating, missing};
+
+//     }
+// }
+public class MissingAndreapeatingnum {
+
+    
+}
+
+
