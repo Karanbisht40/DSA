8e85a2780fcabf6d71f16e7e65940e421e20c24a
Karan Bisht <karanbisht4041@gmail.com>
2026-02-27 09:32:14 +0530

    chn

--- Array/CountSubarrayWithXorasK.java
+++ Array/CountSubarrayWithXorasK.java
@@ -1,3 +1,18 @@
-public class CountSubarrayWithXorasK {
-    
+class CountSubarrayWithXorasK {
+    public int subarraysWithXorK(int[] nums, int k) {
+      Map<Integer, Integer> freq = new HashMap<>();
+      freq.put(0,1);
+      int prefixxor= 0;
+      int cnt =0;
+      for(int i =0; i<nums.length; i++){
+        prefixxor = prefixxor ^ nums[i];
+        int target = prefixxor ^ k;
+
+        if(freq.containsKey(target)){
+            cnt += freq.get(target);
+        }
+        freq.put(prefixxor, freq.getOrDefault(prefixxor,0)+1);
+      }
+      return cnt;
+    }
 }
