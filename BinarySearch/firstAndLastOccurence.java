a0f5804be8320ae7055cfa2abd51b6a655b3533c
Karan Bisht <karanbisht4041@gmail.com>
2026-03-15 12:26:52 +0530

    bs

--- BinarySearch/firstAndLastOccurence.java
+++ BinarySearch/firstAndLastOccurence.java
@@ -46,3 +46,57 @@ public class firstAndLastOccurence {
 //            if(lb ==n || arr[lb] != x)
 //             return (-1, -1);
 //            return{lb, UpperBound(arr, x)-1};
+
+
+//using binary search 
+
+class Solution {
+public int firstOcc(int nums[], int target){
+    int n = nums.length;
+    int low = 0;
+    int high = n-1;
+    int first = -1;
+
+    while(low<= high){
+         int mid = low + (high-low)/2;
+
+         if(nums[mid]== target){
+            first  = mid;
+            high = mid-1;
+         }else if(nums[mid] < target){
+            low = mid +1;
+         } else{
+            high = mid -1;
+         }
+    }
+    return first;
+}
+
+public int lastOcc(int nums[], int target){
+    int n = nums.length;
+    int low = 0;
+     int high = n-1;
+     int last = -1;
+
+     while(low<= high){
+
+        int mid = low+ (high -low)/2;
+
+        if(nums[mid]== target){
+            last = mid;
+            low = mid +1;
+        }else if(nums[mid] < target){
+            low = mid+1;
+     } else{
+        high = mid-1;
+     }
+}
+     return last;
+}
+    public int[] searchRange(int[] nums, int target) {
+        int first = firstOcc(nums, target);
+        if(first== -1) return new int[]{-1,-1};
+        int last = lastOcc(nums, target);
+        return new int[]{first , last};
+    }
+}
