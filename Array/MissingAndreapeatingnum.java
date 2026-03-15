8dbf0cdfc735ba4de371b3858f20b5842936bb70
Karan Bisht <karanbisht4041@gmail.com>
2026-03-15 09:37:49 +0530

    bs

--- Array/MissingAndreapeatingnum.java
+++ Array/MissingAndreapeatingnum.java
@@ -1,5 +1,7 @@
 
-//bettrer sol--
+
+//better solution
+
 // public class MissingAndreapeatingnum {
   
 //     public int[] findMissingRepeatingNumbers(int[] nums) {
@@ -22,34 +24,38 @@
 //     return new int[]{repeating, missing};
 
 //     }
-// }
-// import java.util.*;
-// public class MissingAndreapeatingnum {
 
-//     public int[] findMissingRepeating(int[] nums){
-//      long n = nums.length;
-//      long sn = (n*(n+1)/2);
-//      long s2n = (n*(n-1)*(2*n+1))/6;
-//      long s =0;
-//      long s2= 0;
-//           for (int i = 0; i < n; i++) {
-//             S += nums[i];
-//             S2 += (long) nums[i] * (long) nums[i];
+// }
+
+// optimal solution mathematics base
+
+import java.util.*;
+public class MissingAndreapeatingnum {
+
+    public int[] findMissingRepeating(int[] nums){
+     long n = nums.length;
+     long sn = (n*(n+1)/2);
+     long s2n = (n*(n-1)*(2*n+1))/6;
+     long s =0;
+     long s2= 0;
+          for (int i = 0; i < n; i++) {
+            S += nums[i];
+            S2 += (long) nums[i] * (long) nums[i];
         
-//           // Compute the difference values
-//         long val1 = S - SN; // X - Y
+          // Compute the difference values
+        long val1 = S - SN; // X - Y
 
-//         // S2 - S2n = X^2 - Y^2
-//         long val2 = S2 - S2N;
+        // S2 - S2n = X^2 - Y^2
+        long val2 = S2 - S2N;
 
-//         // Calculate X + Y
-//         val2 = val2 / val1;
+        // Calculate X + Y
+        val2 = val2 / val1;
 
-//         // Calculate X and Y
-//         long x = (val1 + val2) / 2; // repeating
-//         long y = x - val1;          // missing
+        // Calculate X and Y
+        long x = (val1 + val2) / 2; // repeating
+        long y = x - val1;          // missing
 
-//         return new int[]{(int) x, (int) y};
-//           }
-//     }
-// }
+        return new int[]{(int) x, (int) y};
+          }
+    }
+}
