690ed9ffb0ff00e76c43018c1af26fad8817c62b
Karan Bisht <karanbisht4041@gmail.com>
2026-07-23 09:53:41 +0530

    string

---
+++ Array/Practice.java
@@ -0,0 +1,26 @@
+import java.*;
+
+// int n[] = {12,23,21,2,32};
+// int Largest =a[0];
+// int seLargest = -1;
+
+// for(int i=0; i<n; i++){
+//  if(arr[i]> Largest){
+//     seLargest = Largest;
+//     Largest = arr[i];
+// } 
+// } else if(arr[i] <Largest && a[i]> seLargest){
+//     seLargest = arr[i];
+// }
+
+ for(int i=0; i<n; i++){
+    if(arr[i]> arr[i+1]){
+        return false
+    } return true;
+ }
+
+
+a= 12, b=10
+int temp= a[a]
+int a = b;  // a=10
+int b = temp; //12
