c9e3dad2aef7072bd891283b5fc77cf736cfd45c
Karan Bisht <karanbisht4041@gmail.com>
2026-07-24 11:53:33 +0530

    string

---
+++ String/MaxNestingDepthOfParanthesis.java
@@ -0,0 +1,20 @@
+
+//brute 
+class Solution {
+    public int maxDepth(String s) {
+        Stack<Character> st = new Stack<>();
+        int depth = 0;
+
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+
+            if(ch =='('){
+                st.push(ch);
+                depth = Math.max(depth, st.size());
+            }else if(ch == ')'){
+               st.pop();
+            }
+        }
+        return depth;
+    }
+}
